4fa54aacec3eceb303d55f6d66b4168bbbf7c87f
Patrick Dack <patrickdack89@gmail.com>
2022-03-04 14:32:20 -0300

    feat: cria uma api para consulta de cursos

---
+++ src/main/java/com/patrick/crudspring/controllers/CourseController.java
@@ -0,0 +1,26 @@
+package com.patrick.crudspring.controllers;
+
+import java.util.List;
+
+import com.patrick.crudspring.models.Course;
+import com.patrick.crudspring.repository.CourseRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+  private final CourseRepository courseRepository;
+
+  @GetMapping
+  public List<Course> list() {
+    return courseRepository.findAll();
+  }
+
+}
---
+++ src/main/java/com/patrick/crudspring/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.patrick.crudspring.repository;
+
+import com.patrick.crudspring.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long>{
+
+}
